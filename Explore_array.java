7f9244dfa5d40a46a84f0674e3409a19b661c47a
Devika223320 <72210804+Devika223320@users.noreply.github.com>
2020-10-01 20:26:28 +0530

    Update Explore_array.java

--- Explore_array.java
+++ Explore_array.java
@@ -9,15 +9,15 @@ public class Explore_array {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        int arr[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = scan.nextInt();
+        int A[] = new int[n];
+        for (int i=0; i<n; i++) {
+            A[i] = scan.nextInt();
         }
-        for (int i = 0; i < n; i++) {
-            if (arr[i] % 2 == 0) {
-                System.out.print(0 + " ");
+        for (int i=0; i<n; i++) {
+            if (A[i]%2 == 0) {
+                System.out.print(0 + " OK ");
             } else {
-                System.out.print(1 + " ");
+                System.out.print(1 + " NOT OK ");
             }
         }
     }
