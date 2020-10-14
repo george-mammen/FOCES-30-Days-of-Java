9faf7f7f448ebfcdefb1cd6d75ea972460e8cb67
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-10-14 09:18:50 +0530

    Create Array.toString().java

---
+++ Array.toString().java
@@ -0,0 +1,20 @@
+// Day - 12
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n=scan.nextInt();
+        int arr[]= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=i;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
