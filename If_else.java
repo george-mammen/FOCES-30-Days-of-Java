6e6aa541685d4975502a6ae2ec72f3b625e47adc
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-10-14 09:27:51 +0530

    Update If_else.java

--- If_else.java
+++ If_else.java
@@ -1,4 +1,11 @@
-/*Day One*/
+/*Day One
+
+Task : 
+Given an integer, , perform the following conditional actions:
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird */
 
 import java.io.*;
 import java.math.*;
