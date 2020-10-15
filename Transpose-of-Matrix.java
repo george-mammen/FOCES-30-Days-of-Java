fbd47dcd3cbd0bcd2b7040b10d1992c82e15a1c5
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-10-15 09:17:41 +0530

    Create Transpose-of-Matrix.java

---
+++ Transpose-of-Matrix.java
@@ -0,0 +1,31 @@
+/* Day 13
+Task : Given a matrix of dimension m x n, find the transpose of that matrix. */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int r = scan.nextInt();
+        int c = scan.nextInt();
+        int arr[][] = new int[r][c];
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                arr[i][j] = scan.nextInt();
+            }
+        }
+        for(int i = 0; i < c; i++){
+            for(int j = 0; j < r; j++){
+                System.out.print(arr[j][i]+" ");
+            }
+            System.out.println(" ");
+        }
+               
+      
+    }
+}
