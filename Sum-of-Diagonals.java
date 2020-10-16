7e3da6ac07fa895d4698d1a19c3fee4598215031
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-10-16 10:16:14 +0530

    Create Sum-of-Diagonals.java

---
+++ Sum-of-Diagonals.java
@@ -0,0 +1,30 @@
+/* Day 14
+Task : Given a square matrix of dimension n x n, find the product of the sums of the elements along the two diagonals of the matrix. */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int r = scan.nextInt();
+        int sump = 0, sums = 0;
+        int arr[][] = new int[r][r];
+        
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < r; j++){
+                arr[i][j] = scan.nextInt();
+           }
+        }
+        for(int i = 0; i < r; i++){
+            sums += arr[i][r-i-1];
+            sump += arr[i][i];
+            }     
+        
+        System.out.println(sump * sums); 
+}
+}
