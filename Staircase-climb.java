f35f72a9942ce119d7b3842e35289cd7aa1b9e51
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-10-21 11:10:17 +0530

    Create Staircase-climb.java

---
+++ Staircase-climb.java
@@ -0,0 +1,49 @@
+/* Day 17 
+Task : Calculate the number of ways  to climb 'n' stairs */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int stepPerms(int n) {
+        if(n == 0){
+            return 1;
+        }
+        else if(n == 1 || n == 2){
+            return n;
+        }
+        else {
+            return stepPerms(n - 1) + stepPerms(n - 2) + stepPerms(n - 3);
+        }
+        
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int s = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int sItr = 0; sItr < s; sItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int res = stepPerms(n);
+
+            bufferedWriter.write(String.valueOf(res));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
