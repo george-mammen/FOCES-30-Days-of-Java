bd465536238d478a6d76936dd04f2c9ee1ab79ad
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-10-22 10:55:31 +0530

    Create Sum=of-negative-array.java

---
+++ Sum=of-negative-array.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+      
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+        int count = 0;
+        for(int i = 0;i < n;i++){
+            arr[i] = scan.nextInt();
+        }
+        for(int i = 0;i < n;i++){
+            int sum = 0;
+            for(int j = i;j < n;j++){
+                sum = arr[j] + sum;
+                if(sum < 0){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
