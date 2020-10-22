640992c656f8ef8b49e0e5bca71db5a984feac40
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-10-22 12:08:44 +0530

    Update and rename Transpose-of-Matrix.java to DayThirteen.java

--- DayThirteen.java
+++ DayThirteen.java
@@ -7,7 +7,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class DayThirteen {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
